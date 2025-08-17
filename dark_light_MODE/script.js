c476bd91c12428e6f122a9df7c639bf53ec5cc49
pragatiagrawal69 <prag90987@gmail.com>
2025-08-17 10:46:06 +0530

    Update script.js

--- dark_light_MODE/script.js
+++ dark_light_MODE/script.js
@@ -2,25 +2,52 @@ const fullDark = document.querySelector('#full-dark')
 const containerDark = document.querySelector('#container-dark')
 const containerII = document.querySelector('.containerII')
 
-fullDark.addEventListener('change',()=>{
-if(fullDark.checked){
-    document.body.classList.add('dark')
-} else{
-    document.body.classList.remove('dark')
-}
-containerDark.checked=fullDark.checked
-if(containerDark.checked){
-    containerII.classList.add('dark')
-} else{
-    containerII.classList.remove('dark')
-}
-})
+const isFullDark =JSON.parse(localStorage.getItem('fullDark'))
+const isContainerDark =JSON.parse(localStorage.getItem('containerDark'))
 
-
-containerDark.addEventListener('change',()=>{
-if(containerDark.checked){
-    containerII.classList.add('dark')
-} else{
-    containerII.classList.remove('dark')
+if(isFullDark){
+    fullDark.checked = true
 }
+if(isContainerDark){
+    containerDark.checked = true
+}
+   changeFullDarkMode()
+    changeContainedDarkMode()
+
+function changeContainedDarkMode() {
+    // if (containerDark.checked) {
+    //     containerII.classList.add('dark')
+    //     localStorage.setItem('containerDark', true)
+    // } else {
+    //     containerII.classList.remove('dark')
+    //     localStorage.setItem('containerDark', false)
+    // }
+    changeTheme(containerDark,containerII,'containerDark')
+}
+function changeFullDarkMode() {
+    // if (fullDark.checked) {
+    //     document.body.classList.add('dark')
+    //     localStorage.setItem('fullDark', true)
+    // } else {
+    //     document.body.classList.remove('dark')
+    //     localStorage.setItem('fullDark', false)
+    // }
+    // containerDark.checked = fullDark.checked
+    changeTheme(fullDark,document.body,'fullDark')
+}
+
+function changeTheme(checkBox, element, localstorage) {
+    if (checkBox.checked) {
+        element.classList.add('dark')
+        localStorage.setItem(localstorage, true)
+    } else {
+        element.classList.remove('dark')
+        localStorage.setItem(localstorage, false)
+    }
+}
+
+fullDark.addEventListener('change', () => {
+    changeFullDarkMode()
+    changeContainedDarkMode()
 })
+containerDark.addEventListener('change', changeContainedDarkMode)
