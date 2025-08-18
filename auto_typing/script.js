d89490fd2d170049c78bcb22572607fed74fea77
pragatiagrawal69 <prag90987@gmail.com>
2025-08-18 12:14:14 +0530

    Add files via upload

---
+++ auto_typing/script.js
@@ -0,0 +1,40 @@
+
+const span = document.querySelector('span')
+const wordList = ['Web Developer', 'Programmer', 'Creator']
+
+let wordIndex = 0
+let characterIndex = 0
+let skipUpdate = 0
+let reverseType = false
+
+const intervalId = setInterval(() => {
+  if (skipUpdate) {
+    skipUpdate--
+    return
+  }
+
+  if (!reverseType) {
+    skipUpdate = 2
+    span.innerText = span.innerText + wordList[wordIndex][characterIndex]
+    characterIndex++
+  } else {
+    span.innerText = span.innerText.slice(0, span.innerText.length - 1)
+    characterIndex--
+  }
+
+  if (characterIndex === wordList[wordIndex].length) {
+    skipUpdate = 6
+    reverseType = true
+  }
+
+  if (span.innerText.length === 0 && reverseType) {
+    reverseType = false
+    characterIndex = 0
+    wordIndex++
+  }
+  
+  if (wordIndex === wordList.length - 1) {
+    wordIndex = 0
+  }
+
+}, 200);
